a30f9e86657f111a7937271886d05a2beca40bb1
geerrobo <46592198+geerrobo@users.noreply.github.com>
2020-11-16 01:31:25 +0700

    asd

--- compiller.java
+++ compiller.java
@@ -9,7 +9,7 @@ public class compiller {
     String path;
 
     public compiller(String file) throws Exception {
-        path = file;
+        path = file + "assembly.txt";
 
         File fr = new File(path);
         Scanner sc = new Scanner(fr);
@@ -22,13 +22,157 @@ public class compiller {
                 break;
             pc[i] = translate1(pc[i], i);
         }
-        for (int i = 0; i < inst.length; i++) {
+        for (int i = 0; i < pc.length; i++) {
             if (pc[i] == null)
                 break;
             pc[i] = translate2(pc[i], i);
         }
+        for (int i = 0; i < pc.length; i++) {
+            if (pc[i] == null)
+                break;
+            pc[i] = toBinary(pc[i], i);
+        }
 
         printState();
+
+        try {
+            File myObj = new File(file + "machine.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+            FileWriter myWriter = new FileWriter(file + "machine.txt");
+            for (String s : pc) {
+                if(s==null) break;
+                myWriter.write(s+"\n");
+            }
+            myWriter.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private String toBinary(String str, int index) {
+        String temp = "";
+
+        try {
+            String[] array = str.split(" ");
+
+            System.out.println(array[0]);
+
+            switch (array[0]) {
+                case "add":
+                    array[0] = "000";
+                    array[1] = decToBinary(array[1]);
+                    array[2] = decToBinary(array[2]);
+                    array[3] = decToBinary(array[3]);
+                    while (array[3].length() < 16) {
+                        array[3] += "0";
+                    }
+                    break;
+
+                case "nand":
+                    array[0] = "001";
+                    array[1] = decToBinary(array[1]);
+                    array[2] = decToBinary(array[2]);
+                    array[3] = decToBinary(array[3]);
+                    while (array[3].length() < 16) {
+                        array[3] += "0";
+                    }
+                    break;
+
+                case "lw":
+                    array[0] = "010";
+                    array[1] = decToBinary(array[1]);
+                    array[2] = decToBinary(array[2]);
+                    array[3] = decToBinary(array[3]);
+                    while (array[3].length() < 16) {
+                        array[3] += "0";
+                    }
+                    break;
+
+                case "sw":
+                    array[0] = "011";
+                    array[1] = decToBinary(array[1]);
+                    array[2] = decToBinary(array[2]);
+                    array[3] = decToBinary(array[3]);
+                    while (array[3].length() < 16) {
+                        array[3] += "0";
+                    }
+                    break;
+
+                case "beq":
+                    array[0] = "100";
+                    array[1] = decToBinary(array[1]);
+                    array[2] = decToBinary(array[2]);
+                    array[3] = decToBinary(array[3]);
+                    while (array[3].length() < 16) {
+                        array[3] += "0";
+                    }
+                    break;
+
+                case "jalr":
+                    array[0] = "101";
+                    array[1] = decToBinary(array[1]);
+                    array[2] = decToBinary(array[2]);
+                    array[3] = decToBinary(array[3]);
+                    while (array[3].length() < 16) {
+                        array[3] += "0";
+                    }
+                    break;
+
+                case "halt":
+                    array[0] = "110";
+                    while (array[0].length() < 25) {
+                        array[0] += "0";
+                    }
+                    break;
+
+                case "noop":
+                    array[0] = "111";
+                    while (array[0].length() < 25) {
+                        array[0] += "0";
+                    }
+                    break;
+
+                default:
+                    for (int i = 0; i < array.length; i++) {
+                        if (array[i].equals(".fill")) {
+                            for (int j = 0; j <= i; j++) {
+                                array[j] = "0";
+                            }
+                            array[i + 1] = decToBinary(array[i + 1]);
+                        }
+                    }
+                    break;
+            }
+
+            for (String s : array) {
+                while (s.length() < 3) {
+                    s = "0" + s;
+                }
+                temp += s;
+            }
+
+            while (temp.length() < 32) {
+                temp = 0 + temp;
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        return temp;
+    }
+
+    private String decToBinary(String str) {
+        if (Integer.parseInt(str) < 0) {
+            String st = Integer.toBinaryString(Integer.parseInt(str));
+            st = st.substring(st.length() - 16, st.length());
+            return st;
+        } else {
+            return Integer.toBinaryString(Integer.parseInt(str));
+        }
     }
 
     private String translate2(String str, int index) {
@@ -51,7 +195,7 @@ public class compiller {
     private String translate1(String str, int index) {
         String[] array = str.split(" ");
 
-        if (array.length <= 2)
+        if (array.length <= 1)
             return str;
 
         // normal form
@@ -71,26 +215,6 @@ public class compiller {
                     }
                 }
             }
-
-            /*
-             * switch (array[0]) { case "add": array[0] = "000"; break;
-             * 
-             * case "nand": array[0] = "001"; break;
-             * 
-             * case "lw": array[0] = "010"; break;
-             * 
-             * case "sw": array[0] = "011"; break;
-             * 
-             * case "beq": array[0] = "100"; break;
-             * 
-             * case "jalr": array[0] = "101"; break;
-             * 
-             * case "halt": array[0] = "110"; break;
-             * 
-             * case "noop": array[0] = "111"; break;
-             * 
-             * default: break; }
-             */
         } else if (array[1].equals(".fill")) { // variable after .fill
             if (isInteger(array[2])) {
 
@@ -104,6 +228,12 @@ public class compiller {
                     str += s + " ";
                 }
                 return str;
+            }
+        } else {
+            for (String s : array) {
+                if (s.equals("halt")) {
+                    return "halt";
+                }
             }
         }
         return str;
@@ -162,6 +292,15 @@ public class compiller {
 
     private boolean matchinstruc(String str) {
         for (String s : inst) {
+            if (str.matches(s)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean matchinstruc2(String str) {
+        for (String s : inst2) {
             if (str.matches(s)) {
                 return true;
             }
